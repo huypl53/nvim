ba9342b83fa6031209aa370dff64ea4631b9bb41
BTL86 <phamlehuy52@gmail.com>
2023-02-02 08:50:57 +0700

    lspsaga show curor diagnostics

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -22,6 +22,7 @@ saga.setup {
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
