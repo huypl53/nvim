2c671e5901fff7bf39ffc74d27b470064f1795e5
huypl53 <phamlehuy53@gmail.com>
2023-02-09 19:54:39 +0700

    lsp config pyright

--- after/plugin/mason.rc.lua
+++ after/plugin/mason.rc.lua
@@ -4,7 +4,19 @@ local status2, lspconfig = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 
 mason.setup({
+  ensure_installed = {
+    "clangd",
+    "dockerls",
+    "yamlls",
+    "astro",
+    "cssls",
+    "tailwindcss",
+    "sumneko_lua",
+    "flow",
+    "tsserver",
+    "pyright"
 
+  }
 })
 
 lspconfig.setup {
