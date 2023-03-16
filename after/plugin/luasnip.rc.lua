553fbb7b043cf338653cf0b4c01b6e14d8534edf
huypl53 <phamlehuy53@gmail.com>
2023-03-16 22:28:48 +0700

    luasnip react init state

--- after/plugin/luasnip.rc.lua
+++ after/plugin/luasnip.rc.lua
@@ -5,8 +5,8 @@ local t = ls.text_node
 local f = ls.function_node
 
 ls.setup({})
-ls.add_snippets('all', {
-  s("cra", {
+ls.add_snippets("javascript", {
+  s("rcc", {
     t({ "import React from 'react';" }),
     t({ "", "", "" }),
     f(function(args, _, _)
@@ -24,3 +24,21 @@ ls.add_snippets('all', {
     t "export default ", i(1), t ';', i(0)
   })
 })
+
+ls.add_snippets("javascript", {
+  s("rcs", {
+    t({ "\tconst [" }), i(1),
+    f(function(args, _, _)
+      return ", set" .. FirstToUpper(args[1][1]) .. "] = React.useState("
+    end, { 1 }),
+    i(2), t({ ")" }), i(0)
+  })
+})
+
+ls.filetype_extend("typescript", { "javascript" })
+ls.filetype_extend("javascriptreact", { "javascript" })
+ls.filetype_extend("typescriptreact", { "javascript" })
+
+function FirstToUpper(str)
+  return (str:gsub("^%l", string.upper))
+end
