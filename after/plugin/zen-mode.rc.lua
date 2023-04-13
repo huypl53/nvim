efec004ef15b0bb2ad4d6f7815b8c937ce9a9c5a
BTL86 <phamlehuy52@gmail.com>
2023-04-13 11:03:00 +0700

    zen mode mapping

--- after/plugin/zen-mode.rc.lua
+++ after/plugin/zen-mode.rc.lua
@@ -4,4 +4,4 @@ if (not status) then return end
 zenMode.setup {
 }
 
-vim.keymap.set('n', 'so', '<cmd>ZenMode<cr>', { silent = true })
+vim.keymap.set('n', 'sz', '<cmd>ZenMode<cr>', { silent = true })
