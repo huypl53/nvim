46e8d42a3138df275303f544ca57a929023b01af
huypl53 <phamlehuy53@gmail.com>
2023-04-22 22:23:17 +0700

    fix unix mischeck

--- init.lua
+++ init.lua
@@ -6,10 +6,15 @@ require('plugins')
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
+local is_unix = has "unix"
 
 if is_mac then
   require('macos')
 end
 if is_win then
   require('windows')
+end
+
+if is_unix then
+  require('unix')
 end
