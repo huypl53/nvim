6e92c5730f37513ba7337829bbdd7556af286137
huypl53 <phamlehuy53@gmail.com>
2023-05-17 14:08:33 +0700

    zen mode disable twightlight at start

--- after/plugin/zen-mode.rc.lua
+++ after/plugin/zen-mode.rc.lua
@@ -2,6 +2,9 @@ local status, zenMode = pcall(require, "zen-mode")
 if (not status) then return end
 
 zenMode.setup {
+  plugins = {
+    twilight = { enabled = false }
+  }
 }
 
 vim.keymap.set('n', 'sz', '<cmd>ZenMode<cr>', { silent = true })
