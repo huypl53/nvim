3a8e4adca459ddfe3fa1fce8009babef32780c93
huypl53 <phamlehuy52@gmail.com>
2023-09-22 10:33:26 +0700

    rename file update import no need

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -36,42 +36,6 @@ local on_attach = function(client, bufnr)
   --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
 end
 
-local rename_file = function()
-  local source_file, target_file
-
-  vim.ui.input({
-      prompt = "Source : ",
-      completion = "file",
-      default = vim.api.nvim_buf_get_name(0)
-  },
-      function(input)
-          source_file = input
-      end
-  )
-  vim.ui.input({
-      prompt = "Target : ",
-      completion = "file",
-      default = source_file
-  },
-      function(input)
-          target_file = input
-      end
-  )
-
-  local params = {
-      command = "_typescript.applyRenameFile",
-      arguments = {
-          {
-              sourceUri = source_file,
-              targetUri = target_file,
-          },
-      },
-      title = ""
-  }
-
-  vim.lsp.util.rename(source_file, target_file)
-  vim.lsp.buf.execute_command(params)
-end
 
 protocol.CompletionItemKind = {
   '', -- Text
@@ -114,13 +78,6 @@ nvim_lsp.tsserver.setup {
   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "jsx" },
   -- cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities,
-  commands = {
-    RenameFile = {
-      rename_file,
-      description = 'Rename File'
-    }
-  }
-
 }
 
 nvim_lsp.sourcekit.setup {
