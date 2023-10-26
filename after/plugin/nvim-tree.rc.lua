eac0d414af51ca3f032de1e000beb7a7399c226e
huypl53 <phamlehuy52@gmail.com>
2023-10-26 22:21:01 +0700

    nvim-tree fix invalid config

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -4,11 +4,6 @@ if (not status) then return end
 nvimtree.setup({
   view = {
     adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      }
-    }
   },
   renderer = {
     group_empty = true,
