963d0bbd1a457f3cad276a4a3dc08155baaf2754
huypl53 <phamlehuy52@gmail.com>
2024-09-13 08:43:12 +0700

    default lazy plugins for vscode

--- lua/plugins/user/vscode.lua
+++ lua/plugins/user/vscode.lua
@@ -1,3 +1,23 @@
 return {
+  -- by LazyNvim
+  "dial.nvim",
+  "flit.nvim",
+  "lazy.nvim",
+  "leap.nvim",
+  "mini.ai",
+  "mini.comment",
+  "mini.move",
+  "mini.pairs",
+  "mini.surround",
+  "nvim-treesitter",
+  "nvim-treesitter-textobjects",
+  "nvim-ts-context-commentstring",
+  "vim-repeat",
+  "yanky.nvim",
 
+  -- Extra
+  "unblevable/quick-scope",
+  "easymotion/vim-easymotion",
+  "machakann/vim-sandwich",
+  "tpope/vim-commentary"
 }
