a22bc0cb6450d38b5db197d4c64ebc9c364e71b2
huypl53 <phamlehuy52@gmail.com>
2024-10-02 09:21:31 +0700

    feat: register codeium

--- lua/plugins/user/notvscode.lua
+++ lua/plugins/user/notvscode.lua
@@ -67,6 +67,17 @@ return {
 		"folke/zen-mode.nvim",
 		"folke/twilight.nvim",
 		"liuchengxu/vista.vim",
+
+		{
+			"Exafunction/codeium.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"hrsh7th/nvim-cmp",
+			},
+			config = function()
+				require("codeium").setup({})
+			end,
+		},
 		{
 			"iamcco/markdown-preview.nvim",
 			build = function()
