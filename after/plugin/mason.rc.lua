a17d569b29a5b8e7e9dd3185688e61958af88e83
le huy <huypl259@gmail.com>
2024-10-13 16:18:35 +0700

    update mason lspconfig

--- after/plugin/mason.rc.lua
+++ after/plugin/mason.rc.lua
@@ -2,35 +2,268 @@ local status, mason = pcall(require, "mason")
 if not status then
 	return
 end
-local status2, lspconfig = pcall(require, "mason-lspconfig")
+local status2, masonlsp = pcall(require, "mason-lspconfig")
 if not status2 then
 	return
 end
 
-mason.setup({
+mason.setup({})
+
+masonlsp.setup({
 	ensure_installed = {
 		"clangd",
 		"dockerls",
 		"yamlls",
 		"astro",
-		"cssmodules_ls",
 		"cssls",
 		"tailwindcss",
 		"lua_ls",
 		"html",
-		"flow",
 		"ts_ls",
 		"pyright",
-		"prisma-language-server",
+		"prismals",
 		"gopls",
-		"black",
-		"bash-language-server",
+		"bashls",
 		"cssmodules_ls",
 		"emmet_language_server",
-		"beautysh",
+	},
+  automatic_installation = true,
+})
+
+local status, nvim_lsp = pcall(require, "lspconfig")
+if not status then
+	return
+end
+
+local protocol = require("vim.lsp.protocol")
+
+local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local enable_format_on_save = function(_, bufnr)
+	vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		group = augroup_format,
+		buffer = bufnr,
+		callback = function()
+			vim.lsp.buf.format({ bufnr = bufnr })
+		end,
+	})
+end
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+
+	--Enable completion triggered by <c-x><c-o>
+	--local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+	--buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
+
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	--buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	buf_set_keymap("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	--buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+end
+
+protocol.CompletionItemKind = {
+	"", -- Text
+	"", -- Method
+	"", -- Function
+	"", -- Constructor
+	"", -- Field
+	"", -- Variable
+	"", -- Class
+	"ﰮ", -- Interface
+	"", -- Module
+	"", -- Property
+	"", -- Unit
+	"", -- Value
+	"", -- Enum
+	"", -- Keyword
+	"﬌", -- Snippet
+	"", -- Color
+	"", -- File
+	"", -- Reference
+	"", -- Folder
+	"", -- EnumMember
+	"", -- Constant
+	"", -- Struct
+	"", -- Event
+	"ﬦ", -- Operator
+	"", -- TypeParameter
+}
+
+-- Set up completion using nvim_cmp with LSP source
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- nvim_lsp.flow.setup({
+-- 	on_attach = on_attach,
+-- 	capabilities = capabilities,
+-- })
+
+nvim_lsp.ts_ls.setup({
+	on_attach = on_attach,
+	filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "jsx" },
+	-- cmd = { "typescript-language-server", "--stdio" },
+	capabilities = capabilities,
+})
+
+nvim_lsp.sourcekit.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+nvim_lsp.lua_ls.setup({
+	capabilities = capabilities,
+	on_attach = function(client, bufnr)
+		on_attach(client, bufnr)
+		-- enable_format_on_save(client, bufnr)
+	end,
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+		},
 	},
 })
 
-lspconfig.setup({
-	automatic_installation = true,
+nvim_lsp.tailwindcss.setup({
+	on_attach = function(client, bufnr)
+		on_attach(client, bufnr)
+		local status, tailwindcss_color = pcall(require, "tailwindcss-colors")
+		if status then
+			tailwindcss_color.buf_attach(bufnr)
+		end
+	end,
+	capabilities = capabilities,
+})
+
+nvim_lsp.html.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+nvim_lsp.cssmodules_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	init_options = {
+		camelCase = "dashes",
+	},
+	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+})
+
+nvim_lsp.cssls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+nvim_lsp.astro.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+nvim_lsp.pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+nvim_lsp.prismals.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+nvim_lsp.gopls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = nvim_lsp.util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+		},
+	},
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.go",
+	callback = function()
+		local params = vim.lsp.util.make_range_params()
+		params.context = { only = { "source.organizeImports" } }
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+		for cid, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+					vim.lsp.util.apply_workspace_edit(r.edit, enc)
+				end
+			end
+		end
+		vim.lsp.buf.format({ async = false })
+	end,
+})
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	update_in_insert = false,
+	virtual_text = { spacing = 4, prefix = "●" },
+	severity_sort = true,
+})
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "DiagnosticDefault" .. type })
+end
+
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●",
+	},
+	update_in_insert = true,
+	float = {
+		source = "always", -- Or "if_many"
+	},
+})
+
+nvim_lsp.emmet_language_server.setup({
+	filetypes = {
+		"css",
+		"eruby",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"less",
+		"sass",
+		"scss",
+		"pug",
+		"typescriptreact",
+	},
+	init_options = {
+		includeLanguages = {},
+		excludeLanguages = {},
+		extensionsPath = {},
+		preferences = {},
+		showAbbreviationSuggestions = true,
+		showExpandedAbbreviation = "always",
+		showSuggestionsAsSnippets = false,
+		syntaxProfiles = {},
+		variables = {},
+	},
 })
