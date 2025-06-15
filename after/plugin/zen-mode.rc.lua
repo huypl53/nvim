1a61d9a673d83dbea069a02a9c3002494954f8a8
huypl53 <huypl259@gmail.com>
2025-06-15 21:36:14 +0700

    update outline

--- after/plugin/zen-mode.rc.lua
+++ after/plugin/zen-mode.rc.lua
@@ -1,24 +1,10 @@
 local status, zenMode = pcall(require, "zen-mode")
 if (not status) then return end
 
-
-local status_vista, vista = pcall(require, "zen-mode")
-if (not status_vista) then return end
-
 zenMode.setup {
   plugins = {
     twilight = { enabled = false }
   },
-  on_open = function()
-    if status_vista then
-      vim.cmd("Vista!") -- Close vista when entering Zen mode
-    end
-  end,
-  on_close = function()
-    if status_vista then
-      vim.cmd("Vista") -- Reopen vista when exiting Zen mode
-    end
-  end,
 }
 
 vim.keymap.set('n', 'sz', '<cmd>ZenMode<cr>', { silent = true })
--- lua/plugins/user/notvscode.lua
+++ lua/plugins/user/notvscode.lua
@@ -1,133 +1,130 @@
 return {
   {
-    {
-      "folke/tokyonight.nvim",
-      config = function()
-        require("tokyonight").setup({
-          style = "moon",
-          light_style = "night",
-          transparent = true,
-        })
-        vim.cmd([[colorscheme tokyonight]])
-      end,
-    },
-    "nvim-lualine/lualine.nvim", -- Statusline
-    "nvim-lua/plenary.nvim",     -- Common utilities
-    "onsails/lspkind-nvim",      -- vscode-like pictograms
-    "hrsh7th/cmp-buffer",        -- nvim-cmp source for buffer words
-    "hrsh7th/cmp-nvim-lsp",      -- nvim-cmp source for neovim's built-in LSP
-    "hrsh7th/nvim-cmp",          -- Completion
-    -- "jose-elias-alvarez/null-ls.nvim", -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig", -- LSP
-    "nvimdev/lspsaga.nvim",  -- LSP UIs
-    "saadparwaiz1/cmp_luasnip",
-    { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-    {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "moon",
+        light_style = "night",
+        transparent = true,
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  "nvim-lualine/lualine.nvim", -- Statusline
+  "nvim-lua/plenary.nvim",     -- Common utilities
+  "onsails/lspkind-nvim",      -- vscode-like pictograms
+  "hrsh7th/cmp-buffer",        -- nvim-cmp source for buffer words
+  "hrsh7th/cmp-nvim-lsp",      -- nvim-cmp source for neovim's built-in LSP
+  "hrsh7th/nvim-cmp",          -- Completion
+  -- "jose-elias-alvarez/null-ls.nvim", -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig", -- LSP
+  "nvimdev/lspsaga.nvim",  -- LSP UIs
+  "saadparwaiz1/cmp_luasnip",
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
       "nvim-treesitter/nvim-treesitter",
-      build = function()
-        -- require 'nvim-treesitter.install'.compilers = { 'zig',}
-        if vim.fn.has("win32") or vim.fn.has("macunix") then
-          require("nvim-treesitter.install").compilers = { "clang" }
-        else
-          require("nvim-treesitter.install").compilers = { "gcc" }
-        end
-        -- require('nvim-treesitter.install').update({ with_sync = true })
-        require("nvim-treesitter.install").prefer_git = false
-      end,
+      "nvim-tree/nvim-web-devicons"
     },
-    "nvim-tree/nvim-tree.lua",
-    "numToStr/Comment.nvim", -- Comment everything
-    {
-      "antosha417/nvim-lsp-file-operations",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-tree.lua",
-      },
-    },
-    {
-      "folke/todo-comments.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    "kevinhwang91/nvim-bqf",        -- Friendly quickfix
-    "kyazdani42/nvim-web-devicons", -- File icons
-    "lukas-reineke/indent-blankline.nvim",
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = {
-        { "nvim-telescope/telescope-live-grep-args.nvim" },
-        { "MunifTanjim/nui.nvim" },
-        { "nvim-telescope/telescope-file-browser.nvim" },
-      },
-    },
-    "windwp/nvim-autopairs",  -- Auto closing bracket
-    "windwp/nvim-ts-autotag", -- Use treesitter to autoclose and autorename html tag
-    "norcalli/nvim-colorizer.lua",
-    "folke/zen-mode.nvim",    -- Focus on one file
-    "folke/twilight.nvim",    -- Focus on code block
-    {
-      "liuchengxu/vista.vim", -- Require: ctags. TODO: learn more
-      config = function()
-        vim.keymap.set('n', 'go', '<Cmd>Vista!!<CR>', { noremap = true, silent = true })
-        vim.g.vista_stay_on_open = 0
-        vim.g.vista_disable_statusline = 1
-        vim.g.vista_sidebar_keepalt = 1
-        vim.g.vista_echo_cursor = 0
+  },
+  { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      -- require 'nvim-treesitter.install'.compilers = { 'zig',}
+      if vim.fn.has("win32") or vim.fn.has("macunix") then
+        require("nvim-treesitter.install").compilers = { "clang" }
+      else
+        require("nvim-treesitter.install").compilers = { "gcc" }
       end
+      -- require('nvim-treesitter.install').update({ with_sync = true })
+      require("nvim-treesitter.install").prefer_git = false
+    end,
+  },
+  "nvim-tree/nvim-tree.lua",
+  "numToStr/Comment.nvim", -- Comment everything
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
     },
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  "kevinhwang91/nvim-bqf",        -- Friendly quickfix
+  "kyazdani42/nvim-web-devicons", -- File icons
+  "lukas-reineke/indent-blankline.nvim",
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope-live-grep-args.nvim" },
+      { "MunifTanjim/nui.nvim" },
+      { "nvim-telescope/telescope-file-browser.nvim" },
+    },
+  },
+  "windwp/nvim-autopairs",  -- Auto closing bracket
+  "windwp/nvim-ts-autotag", -- Use treesitter to autoclose and autorename html tag
+  "norcalli/nvim-colorizer.lua",
+  "folke/zen-mode.nvim",    -- Focus on one file
+  "folke/twilight.nvim",    -- Focus on code block
 
-    -- {
-    -- 	"Exafunction/codeium.nvim",
-    -- 	dependencies = {
-    -- 		"nvim-lua/plenary.nvim",
-    -- 		"hrsh7th/nvim-cmp",
-    -- 	},
-    -- 	config = function()
-    -- 		require("codeium").setup({})
-    -- 	end,
-    -- },
-    {
-      "iamcco/markdown-preview.nvim",
-      build = function()
-        vim.fn["mkdp#util#install"]()
-      end,
-    },
-    "akinsho/nvim-bufferline.lua",
-    {
-      "chentoast/marks.nvim",
-      event = "VeryLazy",
-      opts = {},
-    },
-    "lewis6991/gitsigns.nvim",
-    "dinhhuy258/git.nvim", -- For git blame & browse
+  -- {
+  -- 	"Exafunction/codeium.nvim",
+  -- 	dependencies = {
+  -- 		"nvim-lua/plenary.nvim",
+  -- 		"hrsh7th/nvim-cmp",
+  -- 	},
+  -- 	config = function()
+  -- 		require("codeium").setup({})
+  -- 	end,
+  -- },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+  "akinsho/nvim-bufferline.lua",
+  {
+    "chentoast/marks.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+  "lewis6991/gitsigns.nvim",
+  "dinhhuy258/git.nvim", -- For git blame & browse
 
-    {
-      "ckipp01/nvim-jenkinsfile-linter",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
+  {
+    "ckipp01/nvim-jenkinsfile-linter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
-    "mfussenegger/nvim-dap",
-    "mfussenegger/nvim-dap-python",
-    "stevearc/conform.nvim", -- Code formatter
-    {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+  "mfussenegger/nvim-dap",
+  "mfussenegger/nvim-dap-python",
+  "stevearc/conform.nvim", -- Code formatter
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
       },
-      keys = {
-        {
-          "<leader>?",
-          function()
-            require("which-key").show({ global = false })
-          end,
-          desc = "Buffer Local Keymaps (which-key)",
-        },
-      },
-    }
+    },
   },
   {
     'kevinhwang91/nvim-ufo', -- Folding like boss
