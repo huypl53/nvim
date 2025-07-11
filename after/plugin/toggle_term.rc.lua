f009ab529b8bfcb16c5469e60b8b4615e20e3617
leekiai <huy@kiaisoft.com>
2025-07-11 11:19:42 +0700

    feat: agent assistant

--- after/plugin/toggle_term.rc.lua
+++ after/plugin/toggle_term.rc.lua
@@ -21,8 +21,37 @@ local lazygit = Terminal:new({
   end,
 })
 
-function _lazygit_toggle()
+function _term_lazygit_toggle()
   lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _term_lazygit_toggle()<CR>", { noremap = true, silent = true })
+
+--#region
+local toggle_float_term = Terminal:new({
+  -- cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+  insert_mappings = true,
+  terminal_mappings = true
+})
+
+function _float_term_toggle()
+  toggle_float_term:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua _float_term_toggle()<CR>", { noremap = true, silent = true })
+
+--#endregion
--- lua/plugins/user/notvscode.lua
+++ lua/plugins/user/notvscode.lua
@@ -12,10 +12,6 @@ return {
   },
   {
     "kyazdani42/nvim-web-devicons",
-    -- defaults = {
-    --   lazy = true,
-    --   version = 'v0.99'
-    -- }
   },                           -- File icons
   "nvim-lualine/lualine.nvim", -- Statusline
   "nvim-lua/plenary.nvim",     -- Common utilities
@@ -81,17 +77,6 @@ return {
   "norcalli/nvim-colorizer.lua",
   "folke/zen-mode.nvim",    -- Focus on one file
   "folke/twilight.nvim",    -- Focus on code block
-
-  -- {
-  -- 	"Exafunction/codeium.nvim",
-  -- 	dependencies = {
-  -- 		"nvim-lua/plenary.nvim",
-  -- 		"hrsh7th/nvim-cmp",
-  -- 	},
-  -- 	config = function()
-  -- 		require("codeium").setup({})
-  -- 	end,
-  -- },
   {
     "iamcco/markdown-preview.nvim",
     build = function()
@@ -119,9 +104,6 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
     },
     keys = {
       {
@@ -151,5 +133,76 @@ return {
       require("pymple").setup()
     end,
   },
-  { 'akinsho/toggleterm.nvim', version = "*", config = true }
+  { 'akinsho/toggleterm.nvim', version = "*", config = true }, -- Terminal inside vim
+  {
+    "yetone/avante.nvim",
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = function()
+      -- conditionally use the correct build system for the current OS
+      if vim.fn.has("win32") == 1 then
+        return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      else
+        return "make"
+      end
+    end,
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "claude",
+      providers = {
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-sonnet-4-20250514",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+      "stevearc/dressing.nvim",        -- for input provider dressing
+      "folke/snacks.nvim",             -- for input provider snacks
+      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",        -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }
 }
