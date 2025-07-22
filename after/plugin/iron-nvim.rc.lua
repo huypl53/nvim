95a0e88050e52d9b77f0400f9858c7295308e001
huypl53 <huypl259@gmail.com>
2025-07-22 21:39:58 +0700

    feat: iron.nvim update

--- after/plugin/iron-nvim.rc.lua
+++ after/plugin/iron-nvim.rc.lua
@@ -32,7 +32,14 @@ iron.setup {
     -- How the repl window will be displayed
     -- See below for more information
     -- repl_open_cmd = view.bottom(40),
-    repl_open_cmd = view.split.vertical.rightbelow("25%"),
+    -- repl_open_cmd = view.split.vertical.rightbelow("25%"),
+    -- repl_open_cmd = view.center("40%", "60%")
+    repl_open_cmd = view.offset {
+      width = math.floor(vim.o.columns * 0.4),
+      height = math.floor(vim.o.lines * 0.8),
+      w_offset = "50%",
+      h_offset = "10%"
+    },
     -- repl_open_cmd can also be an array-style table so that multiple
     -- repl_open_commands can be given.
     -- When repl_open_cmd is given as a table, the first command given will
@@ -51,27 +58,27 @@ iron.setup {
   -- Iron doesn't set keymaps by default anymore.
   -- You can set them here or manually add keymaps to the functions in iron.core
   keymaps = {
-    toggle_repl = "<space>rr", -- toggles the repl open and closed.
+    toggle_repl = "<space>rt", -- toggles the repl open and closed.
     -- If repl_open_command is a table as above, then the following keymaps are
     -- available
     -- toggle_repl_with_cmd_1 = "<space>rv",
     -- toggle_repl_with_cmd_2 = "<space>rh",
-    restart_repl = "<space>sr", -- calls `IronRestart` to restart the repl
-    send_motion = "<space>sc",
-    visual_send = "<space>sc",
-    send_file = "<space>sf",
-    send_line = "<space>sl",
-    send_paragraph = "<space>sp",
-    send_until_cursor = "<space>su",
-    send_mark = "<space>sm",
-    send_code_block = "<space>sb",
-    send_code_block_and_move = "<space>sn",
+    restart_repl = "<space>rr", -- calls `IronRestart` to restart the repl
+    send_motion = "<space>rc",
+    visual_send = "<space>rs",
+    send_file = "<space>rf",
+    send_line = "<space>rl",
+    send_paragraph = "<space>rp",
+    send_until_cursor = "<space>ru",
+    send_mark = "<space>rm",
+    send_code_block = "<space>rb",
+    send_code_block_and_move = "<space>rn",
     mark_motion = "<space>mc",
     mark_visual = "<space>mc",
     remove_mark = "<space>md",
-    cr = "<space>s<cr>",
-    interrupt = "<space>s<space>",
-    exit = "<space>sq",
+    cr = "<space>r<cr>",
+    interrupt = "<space>r<space>",
+    exit = "<space>rq",
     clear = "<space>cl",
   },
   -- If the highlight is on, you can change how it looks
