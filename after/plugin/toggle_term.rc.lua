58f1a0d333e487dc271c7dc4c28d957381cdc673
huypl53 <huypl259@gmail.com>
2025-09-20 22:37:24 +0700

    udpate toggle term

--- after/plugin/toggle_term.rc.lua
+++ after/plugin/toggle_term.rc.lua
@@ -3,6 +3,18 @@ if not status then return end
 
 local Terminal = tg_term.Terminal
 
+local term_status, toggleterm = pcall(require, 'toggleterm')
+if not term_status then return end
+toggleterm.setup({
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.3
+    end
+  end,
+})
+
 local lazygit = Terminal:new({
   cmd = "lazygit",
   dir = "git_dir",
@@ -54,4 +66,29 @@ end
 
 vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua _float_term_toggle()<CR>", { noremap = true, silent = true })
 
+local toggle_vertical_term = Terminal:new({
+  -- cmd = "lazygit",
+  dir = "git_dir",
+  direction = "vertical", -- "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+  insert_mappings = true,
+  terminal_mappings = true
+})
+
+function _vertical_term_toggle()
+  toggle_vertical_term:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua _vertical_term_toggle()<CR>", { noremap = true, silent = true })
 --#endregion
