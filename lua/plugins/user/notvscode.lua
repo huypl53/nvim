4aea68bba707db7505107ce645a758ea115b86f4
kiaisoft <you@example.com>
2025-12-08 09:20:26 +0700

    feat: time machine plugin

--- lua/plugins/user/notvscode.lua
+++ lua/plugins/user/notvscode.lua
@@ -169,5 +169,14 @@ return {
     name = 'window-picker',
     event = 'VeryLazy',
     version = '2.*',
+  },
+  {
+    "y3owk1n/time-machine.nvim",
+    version = "*", -- remove this if you want to use the `main` branch
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
   }
 }
