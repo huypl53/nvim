aff1ac0c93705c10eabbcad690375bccb9a91182
huypl53 <huypl259@gmail.com>
2025-12-31 23:16:06 +0700

    feat: csv viewing

--- lua/plugins/user/notvscode.lua
+++ lua/plugins/user/notvscode.lua
@@ -152,12 +152,32 @@ return {
     version = '2.*',
   },
   {
-    "y3owk1n/time-machine.nvim",
-    version = "*", -- remove this if you want to use the `main` branch
+    "y3owk1n/time-machine.nvim", -- Undo. Redo. Time travel. Take control of your edit history like never before.
+    version = "*",               -- remove this if you want to use the `main` branch
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     }
-  }
+  },
+  "hat0uma/csvview.nvim",
+  ---@module "csvview"
+  ---@type CsvView.Options
+  opts = {
+    parser = { comments = { "#", "//" } },
+    keymaps = {
+      -- Text objects for selecting fields
+      textobject_field_inner = { "if", mode = { "o", "x" } },
+      textobject_field_outer = { "af", mode = { "o", "x" } },
+      -- Excel-like navigation:
+      -- Use <Tab> and <S-Tab> to move horizontally between fields.
+      -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+      -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+      jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+      jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+      jump_next_row = { "<Enter>", mode = { "n", "v" } },
+      jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+    },
+  },
+  cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
 }
