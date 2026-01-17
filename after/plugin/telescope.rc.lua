832dc74160c9acf9ff8e79ef503bfb592c851720
huypl53 <huypl259@gmail.com>
2026-01-17 21:50:23 +0700

    feat: allow telescope to see .venv

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -59,7 +59,7 @@ telescope.setup({
         ["<M-h>"] = actions.results_scrolling_left,
       },
     },
-    file_ignore_patterns = { "node_modules", "yarn.lock", "package-lock.json", "build", "dist", ".git", ".venv" },
+    file_ignore_patterns = { "node_modules", "yarn.lock", "package-lock.json", "build", "dist", ".git", },
     layout_strategy = "flex",
     layout_config = {
       horizontal = {
@@ -271,12 +271,30 @@ telescope.setup({
       -- disables netrw and use telescope-file-browser in its place
       hijack_netrw = true,
       select_buffer = true,
+      respect_gitignore = false,
+      path = "%:p:h",
+      cwd = telescope_buffer_dir(),
+      hidden = { file_browser = false, folder_browser = false },
       mappings = {
         -- your custom insert mode mappings
         ["i"] = {
           ["<C-w>"] = function()
             vim.cmd("normal vbd")
           end,
+          ["<A-c>"] = fb_actions.create,
+          ["<S-CR>"] = fb_actions.create_from_prompt,
+          ["<A-r>"] = fb_actions.rename,
+          ["<A-m>"] = fb_actions.move,
+          ["<A-y>"] = fb_actions.copy,
+          ["<A-d>"] = fb_actions.remove,
+          ["<C-o>"] = fb_actions.open,
+          ["<C-g>"] = fb_actions.goto_parent_dir,
+          ["<C-e>"] = fb_actions.goto_home_dir,
+          ["<C-t>"] = fb_actions.change_cwd,
+          ["<C-f>"] = fb_actions.toggle_browser,
+          ["<C-h>"] = fb_actions.toggle_hidden,
+          ["<C-s>"] = fb_actions.toggle_all,
+          ["<bs>"] = fb_actions.backspace,
         },
         ["n"] = {
           -- your custom normal mode mappings
@@ -285,6 +303,19 @@ telescope.setup({
           ["/"] = function()
             vim.cmd("startinsert")
           end,
+          ["c"] = fb_actions.create,
+          ["r"] = fb_actions.rename,
+          ["m"] = fb_actions.move,
+          ["y"] = fb_actions.copy,
+          ["d"] = fb_actions.remove,
+          ["o"] = fb_actions.open,
+          ["g"] = fb_actions.goto_parent_dir,
+          ["e"] = fb_actions.goto_home_dir,
+          ["w"] = fb_actions.goto_cwd,
+          ["t"] = fb_actions.change_cwd,
+          ["f"] = fb_actions.toggle_browser,
+          ["<C-h>"] = fb_actions.toggle_hidden,
+          ["s"] = fb_actions.toggle_all,
         },
       },
     },
@@ -378,7 +409,7 @@ vim.keymap.set("n", "sf", function()
   telescope.extensions.file_browser.file_browser({
     path = "%:p:h",
     cwd = telescope_buffer_dir(),
-    respect_gitignore = true,
+    respect_gitignore = false,
     hidden = true,
     grouped = true,
     previewer = true,
@@ -396,7 +427,7 @@ vim.keymap.set("n", "sf", function()
       return true
     end,
   })
-end)
+end, { desc = 'Telescope File browser' })
 
 vim.keymap.set("n", "<leader>/", function()
   builtin.keymaps()
