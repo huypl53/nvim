5af268301d2969219bf5e96dcc46d91cf4e0024c
lee <example@mail.com>
2026-08-02 23:28:31 +0700

    feat: update md preview server

--- lua/plugins/user/notvscode.lua
+++ lua/plugins/user/notvscode.lua
@@ -191,9 +191,10 @@ return {
 				-- all optional; sane defaults shown
 				instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
 				port = 0, -- 0 = auto (8421 for takeover, OS-assigned for multi)
-				host = '0.0.0.0',
-				open_browser = false,
+				host = "0.0.0.0",
+				open_browser = true,
 				debounce_ms = 300,
+				default_theme = "light",
 			})
 		end,
 	},
